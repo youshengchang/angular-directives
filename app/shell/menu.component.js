5d50d8f62845ffa88e3ee3c543dbdc017402151c
youshengchang <changyousheng@yahoo.com>
2017-01-18 20:29:16 +0000

    minor

--- app/shell/menu.component.js
+++ app/shell/menu.component.js
@@ -15,7 +15,7 @@ var MenuComponent = (function () {
     MenuComponent = __decorate([
         core_1.Component({
             selector: 'app-menu',
-            template: "\n            <a class=\"mdl-navigation__link\" href=\"\"><i class=\"mdl-color-text--blue-grey-400 material-icons\" role=\"presentation\">home</i>Home</a>\n            <a class=\"mdl-navigation__link\" href=\"\"><i class=\"mdl-color-text--blue-grey-400 material-icons\" role=\"presentation\">inbox</i>Inbox</a>\n            <div class=\"mdl-layout-spacer\"></div>\n            <a class=\"mdl-navigation__link\" href=\"\"><i class=\"mdl-color-text--blue-grey-400 material-icons\" role=\"presentation\">help_outline</i><span class=\"visuallyhidden\">Help</span></a>\n"
+            template: "\n            <a class=\"mdl-navigation__link\" href=\"\" [routerLink]=\"['']\" ><i class=\"mdl-color-text--blue-grey-400 material-icons\" role=\"presentation\">home</i>Home</a>\n            <a class=\"mdl-navigation__link\" href=\"\" [routerLink]=\"['configure']\"><i class=\"mdl-color-text--blue-grey-400 material-icons\" role=\"presentation\">inbox</i>Configure</a>\n            <div class=\"mdl-layout-spacer\"></div>\n            <a class=\"mdl-navigation__link\" href=\"\"><i class=\"mdl-color-text--blue-grey-400 material-icons\" role=\"presentation\">help_outline</i><span class=\"visuallyhidden\">Help</span></a>\n"
         }), 
         __metadata('design:paramtypes', [])
     ], MenuComponent);
--- app/shell/menu.component.ts
+++ app/shell/menu.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import {  RouterModule, Routes } from '@angular/router';
 
 @Component({
 selector: 'app-menu',
 template: `
-            <a class="mdl-navigation__link" href=""><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">home</i>Home</a>
-            <a class="mdl-navigation__link" href=""><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">inbox</i>Inbox</a>
+            <a class="mdl-navigation__link" href="" [routerLink]="['']" ><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">home</i>Home</a>
+            <a class="mdl-navigation__link" href="" [routerLink]="['configure']"><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">inbox</i>Configure</a>
             <div class="mdl-layout-spacer"></div>
             <a class="mdl-navigation__link" href=""><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">help_outline</i><span class="visuallyhidden">Help</span></a>
 `
